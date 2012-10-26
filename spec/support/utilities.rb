0006ac869ea37662c9b4029d70315b83753ef06d
Nathan <nathan.mckaskle@gmail.com>
2012-10-26 15:45:23 -0400

    Finish user edit, update, index and destroy actions

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,13 +1,38 @@
 include ApplicationHelper
 
 def valid_signin(user)
+  visit signin_path
   fill_in "Email",    with: user.email
   fill_in "Password", with: user.password
   click_button "Sign in"
+  cookies[:remember_token] = user.remember_token
 end
 
 RSpec::Matchers.define :have_error_message do |message|
   match do |page|
     page.should have_selector('div.alert.alert-error', text: message)
   end
+end
+
+RSpec::Matchers.define :have_title do |title|
+  match do |page|
+    page.should have_selector('title', text: title)
+    page.should have_selector('h1', text: title)
+  end
+end
+
+RSpec::Matchers.define :have_full_title do |title|
+  match do |page|
+    page.should have_selector('title', text: full_title(title))
+    page.should have_selector('h1', text: title)
+  end
+end
+
+def sign_in(user)
+  visit signin_path
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+  # Sign in when not using Capybara as well.
+  cookies[:remember_token] = user.remember_token
 end
